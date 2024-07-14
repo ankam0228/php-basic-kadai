a911d6f7b2e366e82632b57eab36214f76a626e3
ankam0228 <162595831+ankam0228@users.noreply.github.com>
2024-07-14 21:40:37 +0900

    Add files via upload

---
+++ kadai_007/associat-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>7章課題　連想配列を作って値とキーを出力しよう</title>
+    </head>
+    <body>
+        <?php
+        $date = ['name'=>'onion','price'=>'200','weight'=>'160'];
+        print_r($date);
+        ?>
+    </body>
+</html>
